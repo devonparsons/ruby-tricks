129407c0b0414a2bcae9759f8df0c381a13b5322
Devon Parsons <devon.parsons@mmspos.com>
2015-01-28 15:30:31 -0400

    Add some more tricks

---
+++ Dynamic Method Creation.rb
@@ -0,0 +1,23 @@
+# Comment: If you have a ton of methods that are all very similar, you can define them all without repeating yourself
+
+class Foo
+  def initialize
+    super
+    [:first,:second,:third].each do |method|
+       self.class.send(:define_method, method) do |argument|
+        do_work(method, argument)
+      end
+    end
+  end
+
+  def do_work (one, two)
+    puts "#{one} #{two}"
+  end
+end
+
+foo = Foo.new
+
+puts foo.methods 
+foo.first("The first method!")
+foo.second("And the second!")
+foo.third("Finally, the third!")
