064af3458becf2d9dcfd88293cc6967651da4807
Devon Parsons <devon.parsons@mmspos.com>
2015-02-19 15:35:59 -0400

    New trick

---
+++ tricks/intermediate/discard_garbage_variables.rb
@@ -0,0 +1,71 @@
+# Comment: Ruby honors the convention of using `_` as a placeholder for garbage data
+
+class ThirdPartyWorker
+  attr_reader :title, :sizes, :details
+  def initialize(title, sizes = [], details = {})
+    @title = title
+    @sizes = sizes
+    @details = details
+  end
+end
+
+# None of my implementations of workers care about the sizes or details attribute that workers usually have
+
+class WorkingWorker < ThirdPartyWorker
+  def initialize(title, _, _)
+    super(title, nil, nil)
+  end
+end
+
+# UninterpretableWorker doesn't even make it past the interpreter and cannot be included.
+
+# class UninterpretableWorker <  ThirdPartyWorker
+#   def initialize(title, unused, unused)
+#     super(title)
+#   end
+# end
+
+# UglyWorker works, but rubocop complains about unused variables
+# W: Unused method argument - sizes
+# W: Unused method argument - details
+
+class UglyWorker < ThirdPartyWorker
+  def initialize(title, sizes, details)
+    super(title, nil, nil)
+  end
+end
+
+# CarelessWorker initially appears to work but can't be initialized like any other worker
+
+class CarelessWorker < ThirdPartyWorker
+  def initialize(title)
+    super(title, nil, nil)
+  end
+end
+
+class BlissfullyUnawareWorkerCreator
+  def self.create(worker_class)
+    worker = worker_class.new("valueable text", [12, 32], name: "foo", location: "Florida")
+
+    p worker.title
+    p worker.sizes
+    p worker.details
+  rescue StandardError => e
+    puts "Error! #{e}"
+  end
+end
+
+puts "Create a ThirdPartyWorker:"
+BlissfullyUnawareWorkerCreator.create(ThirdPartyWorker)
+
+puts "\nCreate a WorkingWorker:"
+BlissfullyUnawareWorkerCreator.create(WorkingWorker)
+
+# puts "\nCreate an UninterpretableWorker:"
+# BlissfullyUnawareWorkerCreator.create(UninterpretableWorker)
+
+puts "\nCreate an UglyWorker:"
+BlissfullyUnawareWorkerCreator.create(UglyWorker)
+
+puts "\nCreate a CarelessWorker:"
+BlissfullyUnawareWorkerCreator.create(CarelessWorker)
