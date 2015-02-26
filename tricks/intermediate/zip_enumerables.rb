394956bc919bc8e91e46f60a2c242d36f496bc7e
Devon Parsons <devjrp@gmail.com>
2015-02-26 08:36:43 -0400

    Tighten up code

--- tricks/intermediate/zip_enumerables.rb
+++ tricks/intermediate/zip_enumerables.rb
@@ -1,20 +1,23 @@
 # Comment: You can iterate through any number of Enumerables in lockstep
+# Comment: Note that the length is limited to the zip initiator's length
 
-e = {
+data = {
+  months:    %w(Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec),
+  letters:   %w(a b c d e f g h i j k l m n o p q r s t u v w x y z),
   numbers:   1..10,
-  letters:   %w(a b c d e f g h i j),
-  notes:     %w(do re mi fa so la ti do),
   phonetic:  %w(alpha bravo charlie delta echo),
-  rainbow:   %w(red orange yellow green blue indigo violet)
+  rainbow:   %w(red orange yellow green blue indigo violet),
+  notes:     %w(do re mi fa so la ti).cycle.take(8),
+  evens:     (2..1_000).step(2),
 }
 
 # Titles
-puts e.keys.map{|k|k.capitalize}.join(" ")
-# Values
-e[:numbers].zip(*e.values[1..-1]).each do |layer|
-  layer.zip(e.keys).each do |node|
-    print node[0].to_s.rjust(node[1].length), " "
-  end
-  puts 
-end
+puts data.keys.map(&:capitalize).join(" ")
 
+# Values
+data.first[1].zip(*data.values[1..-1]) do |layer|
+  layer.zip(data.keys) do |value, header|
+    print value.to_s.rjust(header.length), " "
+  end
+  puts
+end
